97f389b9dbe1cbe973aef75858e9d260f2bb17e5
Bin Xia <binxi@microsoft.com>
2015-10-01 14:30:21 +0000

    Refine codes

--- azure_client/helpers.go
+++ azure_client/helpers.go
@@ -15,28 +15,27 @@ func NewServicePrincipalTokenFromCredentials(c map[string]string, scope string) 
 
 // LoadAzureCredentials reads credentials from environment variables
 func LoadAzureCredentials() (map[string]string, error) {
-	credentials := make(map[string]string)
-
 	subscriptionId := os.Getenv("subscriptionID")
 	if subscriptionId == "" {
-		return credentials, errors.New("No subscriptionID provided in environment variables")
+		return nil, errors.New("No subscriptionID provided in environment variables")
 	}
 
 	tenantId := os.Getenv("tenantID")
 	if tenantId == "" {
-		return credentials, errors.New("No tenantID provided in environment variables")
+		return nil, errors.New("No tenantID provided in environment variables")
 	}
 
 	clientId := os.Getenv("clientID")
 	if clientId == "" {
-		return credentials, errors.New("No clientID provided in environment variables")
+		return nil, errors.New("No clientID provided in environment variables")
 	}
 
 	clientSecret := os.Getenv("clientSecret")
 	if clientSecret == "" {
-		return credentials, errors.New("No clientSecret provided in environment variables")
+		return nil, errors.New("No clientSecret provided in environment variables")
 	}
 
+	credentials := make(map[string]string)
 	credentials["subscriptionID"] = subscriptionId
 	credentials["tenantID"] = tenantId
 	credentials["clientID"] = clientId
